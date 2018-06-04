be104bc230c887593c202ac6b744ec1698411e56
yihwan <yihwankim@gmail.com>
2018-06-04 13:02:48 -0700

    add image functionality

--- app.js
+++ app.js
@@ -1,17 +1,45 @@
 var inputs = [];
 const mrRobot = 'mrrobot';
+const got = 'gameofthrones';
 
 function checkShows() {
   var parsed = inputs.filter((str) => /\S/.test(str)).join('').toLowerCase();
 
   if (parsed.includes(mrRobot)) {
-    cornify_add();
     inputs = [];
+    addImage(mrRobot);
   }
+
+  if (parsed.includes(got)) {
+    inputs = [];
+    addImage(got);
+  }
+}
+
+function addImage(show) {
+  var image = document.createElement('img');
+  var [docWidth, docHeight] = [document.body.clientWidth, document.body.clientHeight];
+
+  image.setAttribute('src', `assets/images/${show}.gif`);
+
+  document.body.appendChild(image);
+
+  image.addEventListener('load', function() {
+    var randX = (Math.random() * docWidth) - image.width;
+    var randY = (Math.random() * docHeight) - image.height;
+
+    randX = randX < 0 ? 0 : randX;
+    randY = randY < 0 ? 0 : randY;
+
+    var randPos = [ randX, randY ];
+
+    image.style.position = 'absolute';
+    image.style.left = `${randPos[0]}px`;
+    image.style.top = `${randPos[1]}px`;
+  });
 }
 
 window.addEventListener('keyup', (e) => {
   inputs.push(e.key);
   checkShows();
-  console.log(inputs);
 });
